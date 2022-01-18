dcf4c6b2fba2971e36183e9d2bf341eda7d7735b
David Beazley <dave@dabeaz.com>
2022-01-18 13:13:27 -0600

    Initial interpreter

--- lox.py
+++ lox.py
@@ -2,20 +2,31 @@
 #
 # Main program
 
+import sys
+
 import loxscan
 import loxparse
 import loxast
+import loxinterp
 
 def main(argv):
-    if len(argv) != 2:
+    if len(argv) > 2:
         raise SystemExit(f'Usage: lox.py filename')
-    with open(argv[1]) as file:
-        source = file.read()
+    if len(sys.argv) == 2:
+        with open(argv[1]) as file:
+            source = file.read()
+    else:
+        source = input("Lox > ")
         
     lexer = loxscan.LoxLexer()
     parser = loxparse.LoxParser()
     ast = parser.parse(lexer.tokenize(source))
     print(loxast.ASTPrinter().visit(ast))
+
+    print("::: Running :::")
+    interp = loxinterp.LoxInterpreter()
+    interp.visit(ast)
+    
 
 if __name__ == '__main__':
     import sys
---
+++ loxinterp.py
@@ -0,0 +1,80 @@
+# loxinterp.py
+#
+# Tree-walking interpreter
+
+from loxast import NodeVisitor
+
+# Lox truthiness.  See pg. 101. 
+def _is_truthy(value):
+    if value is None:
+        return False
+    elif isinstance(value, bool):
+        return value
+    else:
+        return True
+
+def _check_numeric_operands(op, left, right):
+    if isinstance(left, float) and isinstance(right, float):
+        return True
+    else:
+        raise RuntimeError(f"{op} operands must be numbers")
+
+def _check_numeric_operand(value):
+    if isinstance(value, float):
+        return True
+    else:
+        raise RuntimeError(f"{op} operand must be a number")
+    
+class LoxInterpreter(NodeVisitor):
+    def visit_Literal(self, node):
+        return node.value
+
+    def visit_Binary(self, node):
+        left = self.visit(node.left)
+        right = self.visit(node.right)
+        if node.op == '+':
+            (isinstance(left, str) and isinstance(right, str)) or _check_numeric_operands(node.op, left, right)
+            return left + right
+        elif node.op == '-':
+            _check_numeric_operands(node.op, left, right)
+            return left - right
+        elif node.op == '*':
+            _check_numeric_operands(node.op, left, right)            
+            return left * right
+        elif node.op == '/':
+            _check_numeric_operands(node.op, left, right)            
+            return left / right
+        elif node.op == '==':
+            return left == right
+        elif node.op == '<':
+            _check_numeric_operands(node.op, left, right)            
+            return left < right
+        elif node.op == '>':
+            _check_numeric_operands(node.op, left, right)            
+            return left > right
+        elif node.op == '<=':
+            _check_numeric_operands(node.op, left, right)            
+            return left <= right
+        elif node.op == '>=':
+            _check_numeric_operands(node.op, left, right)            
+            return left >= right
+        else:
+            raise TypeError(f"Bad operator {node.op}")
+
+    def visit_Unary(self, node):
+        operand = self.visit(node.operand)
+        if node.op == "-":
+            _check_numeric_operand(node.op, operand)
+            return -operand
+        elif node.op == "!":
+            return not _is_truthy(operand)
+        else:
+            raise TypeError(f"Bad operator {node.op}")
+
+    def visit_Grouping(self, node):
+        return self.visit(node.value)
+
+    def visit_Print(self, node):
+        print(self.visit(node.value))
+        
+            
