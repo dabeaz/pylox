801b3fed500506e4cb06a871a8fabef1abc2dba3
David Beazley <dave@dabeaz.com>
2022-01-18 10:09:45 -0600

    Start of AST

---
+++ loxast.py
@@ -0,0 +1,39 @@
+# loxast.py
+
+class Node:
+    def __repr__(self):
+        args = ', '.join(f'{key}={value!r}' for key, value in vars(self).items())
+        return f'{type(self).__name__}({args})'
+
+class Expression(Node):
+    pass
+
+class Literal(Expression):
+    def __init__(self, value):
+        self.value = value
+
+class Binary(Expression):
+    def __init__(self, left, op, right):
+        self.left = left
+        self.op = op
+        self.right = right
+
+class Unary(Expression):
+    def __init__(self, op, operand):
+        self.op = op
+        self.operand = operand
+
+class Grouping(Expression):
+    def __init__(self, value):
+        self.value = value
+
+class Statement(Node):
+    pass
+
+class Print(Statement):
+    def __init__(self, value):
+        self.value = value
+
+        
+
+
